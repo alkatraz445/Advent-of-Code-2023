ffd155ec104009b2c657a55a1535bdef8bacb7c1
Jakub Kraus <jakubkraus@proton.me>
2023-12-16 17:46:18 +0100

    Continuing WIP

--- Day_2/day2.py
+++ Day_2/day2.py
@@ -1,13 +1,18 @@
 # Zlicz ID gier, w których występują TYLKO 12 czerwone kostki, 13 zielone, 14 niebieskie.
-
+# Chcemy podzielić string na 
+# [[Game 1],[zawartość gier]]
 with open("day2_test.txt", "r") as data:
+    split_data = [line.rstrip('\n') for line in data]
     sum_of_id = []
-    test=[]
-    test2=[]
-    for game in data:
-        test.append(game.splitlines())
-    for play in test:
-        print(len(play))
-        # test2.append(play.split(";"))
-     # print(test)
+    data_table = [line.split(':') for line in split_data]
+    game_id = []
+    game_content = []
+
+    for game in data_table:
+        game_id.append(int(game[0][-1]))
+        game_content.append(game[1].replace(" ",""))
+
+    # print(game_data)
+    print(game_content)
+        
 
