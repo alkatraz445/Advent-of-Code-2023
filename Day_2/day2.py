af653e88db29baa33ec90d6b4684a0511627fcc6
Jakub Kraus <jakubkraus@proton.me>
2023-12-17 02:29:27 +0100

    First part done

--- Day_2/day2.py
+++ Day_2/day2.py
@@ -1,18 +1,30 @@
 # Zlicz ID gier, w których występują TYLKO 12 czerwone kostki, 13 zielone, 14 niebieskie.
 # Chcemy podzielić string na 
 # [[Game 1],[zawartość gier]]
-with open("day2_test.txt", "r") as data:
+
+import re
+
+with open("day2.txt", "r") as data:
     split_data = [line.rstrip('\n') for line in data]
-    sum_of_id = []
     data_table = [line.split(':') for line in split_data]
-    game_id = []
     game_content = []
-
+    cubes = {}
+    result = 0
+    max_cubes = {'red': 12, 'green': 13, 'blue': 14}
     for game in data_table:
-        game_id.append(int(game[0][-1]))
+        game_id = re.findall(r'\d+', game[0])
         game_content.append(game[1].replace(" ",""))
-
-    # print(game_data)
-    print(game_content)
-        
+        cubes[int(game_id[0])] = re.findall(r'(\d+|[^\d,;]+)',game[1].replace(" ", ""))
+    
+    for dkey in cubes:
+        i = 0
+        check = True
+        while len(cubes[dkey]) > i:
+            if cubes[dkey][i].isdigit():
+                if int(cubes[dkey][i]) > max_cubes[cubes[dkey][i+1]]:
+                    check = False
+            i += 1                        
+        if check:
+            result += dkey
+    print(result)    
 
